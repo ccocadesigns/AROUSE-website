7f1528716ff14a136a1c9b0ff6467b0d18028785
Leo Tombragel <tombragel.7@buckeyemail.osu.edu>
2026-02-22 14:17:17 -0500

    Finished shows page, with room for header + footer

--- src/main.js
+++ src/main.js
@@ -1,24 +1,131 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+
+ /*document.querySelectorAll('.button').forEach(btn => {
+  btn.addEventListener('click', () => {
+    alert('Clicked ' + btn.textContent);
+  });
+});
+*/
+
+
+/* each view more gets its own button, for each day of the week*/
+const sundayButton = document.getElementById("sun-view-more");
+sundayButton.addEventListener("click", function() {
+  const shows = document.querySelectorAll(".sun-more-shows-container");
+
+   shows.forEach(function(show) {
+
+    if(show.style.display === ""){
+      show.style.display = "flex";
+      sundayButton.textContent = "View Less";
+    } else if(show.style.display === "flex"){
+      show.style.display = "";
+      sundayButton.textContent = "View More?";
+    }
+  
+  });
+});
+
+const mondayButton = document.getElementById("mon-view-more");
+mondayButton.addEventListener("click", function() {
+  const shows = document.querySelectorAll(".mon-more-shows-container");
+
+   shows.forEach(function(show) {
+
+    if(show.style.display === ""){
+      show.style.display = "flex";
+      mondayButton.textContent = "View Less";
+    } else if(show.style.display === "flex"){
+      show.style.display = "";
+      mondayButton.textContent = "View More?";
+    }
+  
+  });
+});
+
+const tuesdayButton = document.getElementById("tue-view-more");
+tuesdayButton.addEventListener("click", function() {
+  const shows = document.querySelectorAll(".tue-more-shows-container");
+
+   shows.forEach(function(show) {
+
+    if(show.style.display === ""){
+      show.style.display = "flex";
+      tuesdayButton.textContent = "View Less";
+    } else if(show.style.display === "flex"){
+      show.style.display = "";
+      tuesdayButton.textContent = "View More?";
+    }
+  
+  });
+});
+
+const wednesdayButton = document.getElementById("wed-view-more");
+wednesdayButton.addEventListener("click", function() {
+  const shows = document.querySelectorAll(".wed-more-shows-container");
+
+   shows.forEach(function(show) {
+
+    if(show.style.display === ""){
+      show.style.display = "flex";
+      wednesdayButton.textContent = "View Less";
+    } else if(show.style.display === "flex"){
+      show.style.display = "";
+      wednesdayButton.textContent = "View More?";
+    }
+  
+  });
+});
+
+const thursdayButton = document.getElementById("thu-view-more");
+thursdayButton.addEventListener("click", function() {
+  const shows = document.querySelectorAll(".thu-more-shows-container");
+
+   shows.forEach(function(show) {
+
+    if(show.style.display === ""){
+      show.style.display = "flex";
+      thursdayButton.textContent = "View Less";
+    } else if(show.style.display === "flex"){
+      show.style.display = "";
+      thursdayButton.textContent = "View More?";
+    }
+  
+  });
+});
+
+const fridayButton = document.getElementById("fri-view-more");
+fridayButton.addEventListener("click", function() {
+  const shows = document.querySelectorAll(".fri-more-shows-container");
+
+   shows.forEach(function(show) {
+
+    if(show.style.display === ""){
+      show.style.display = "flex";
+      fridayButton.textContent = "View Less";
+    } else if(show.style.display === "flex"){
+      show.style.display = "";
+      fridayButton.textContent = "View More?";
+    }
+  
+  });
+});
+
+const saturdayButton = document.getElementById("sat-view-more");
+saturdayButton.addEventListener("click", function() {
+  const shows = document.querySelectorAll(".sat-more-shows-container");
+
+   shows.forEach(function(show) {
+
+    if(show.style.display === ""){
+      show.style.display = "flex";
+      saturdayButton.textContent = "View Less";
+    } else if(show.style.display === "flex"){
+      show.style.display = "";
+      saturdayButton.textContent = "View More?";
+    }
+  
+  });
+});
